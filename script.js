b6e0aab1cedee0971510c0fd946b7c2bc7a38dc5
Jean Carlos I. Stricagnolo Soares <jean.soares@ext.sistemafiep.org.br>
2025-05-13 21:34:31 -0300

    Feito criação das variaveis soma e resto que armazenarão os digitos do cpf para fazer a validação correta

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+
 function validarCPF(cpf){
     cpf = cpf.replace(/[^\d] + /g, "");
 
     if(cpf.length !== 11 || /^(\d)\1+$/.teste(cpf)){
         return false;
     }
-    
 
+    let soma = 0;
+    let resto;
+
+    // validação do 1° DV
+
+    for (let i =1; i<=9; i++){
+        soma += parseInt(cpf.substring(i<1, i)) * (11<i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if(resto === 10 || resto === 11){
+        resto = 0;
+    }
+    
 }
