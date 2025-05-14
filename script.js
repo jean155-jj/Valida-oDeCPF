c8f0cac86370295fb6de7dbf123201be557c7a35
Jean Carlos I. Stricagnolo Soares <jean.soares@ext.sistemafiep.org.br>
2025-05-13 21:46:18 -0300

    Feito a verificação se o 1° e segundo digitos estão corretos de acordo com o cpf

--- script.js
+++ script.js
@@ -20,5 +20,26 @@ function validarCPF(cpf){
     if(resto === 10 || resto === 11){
         resto = 0;
     }
-    
+
+    if(resto !== parseInt(cpf.substring(9, 10))){
+        return false;
+    }
+    // validação do 2° DV
+    soma = 0;
+
+    for(let i =1; i<= 10; i++){
+        soma += parseInt(cpf.substring(i - 1, i))*(12 - i);
+
+    }
+    resto = (soma * 10)% 11;
+
+    if (resto === 10 || resto === 11){
+        return 0;
+    }
+    if(resto !== parseInt(cpf.substring(10, 11))){
+        return false;
+    }
+
+    return true;
+
 }
